beee11e73837262ab581f9b48183d5cbf9d20c13
MANISH50 <manishkumar.ranchi007@gmail.com>
2017-02-11 13:00:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
 console.log('Loaded!');
+var GET =  document.getElementById('move');
+         alert('JAVASCRIPT');
+         GET.onclick = function(){
+             GET.style.marginLeft = '100px';
+         };
+        
