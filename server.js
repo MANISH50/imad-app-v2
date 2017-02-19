eaac2ae25811f9744037558531d00e734ebc2c7c
MANISH50 <manishkumar.ranchi007@gmail.com>
2017-02-19 23:13:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html')); //When get is made to slash the function get executed.
 });
 app.get('/file1',function(res,req){
-   res.sendFile(path.join(__dirname, 'ui', 'file1.html')); 
+   res.send('TOUR PAGE.'); 
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
