08baadba475fcc40301f4443db66cce6c093d38b
MANISH50 <manishkumar.ranchi007@gmail.com>
2017-02-19 23:57:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/myarc',function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'myarc.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
